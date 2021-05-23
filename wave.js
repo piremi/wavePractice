f7ab76579845295238029bea7af4e54f1219955d
Jimmy <piremi@gmail.com>
2021-05-22 17:48:19 -0700

    point and wave update

--- wave.js
+++ wave.js
@@ -1,10 +1,10 @@
 import { Point } from "./point.js";
 export class Wave {
-  constructor() {
-    // this.index = index;
-    // this.totalPoints = totalPoints;
-    // this.color = color;
-    // this.points = [];
+  constructor(index, totalPoints, color) {
+    this.index = index;
+    this.totalPoints = totalPoints;
+    this.color = color;
+    this.points = [];
   }
 
   resize(stageWidth, stageHeight) {
@@ -14,49 +14,45 @@ export class Wave {
     this.centerX = stageWidth / 2;
     this.centerY = stageHeight / 2;
 
+    this.pointGap = this.stageWidth / (this.totalPoints - 1);
+
     this.init();
   }
   init() {
-    this.point = new Point(this.centerX, this.centerY);
-    //this.points = [];
+    //this.point = new Point(this.centerX, this.centerY);
+    this.points = [];
 
-    // for (let i = 0; i < this.totalPoints; i++) {
-    //   const point = new Point(this.index + i, this.pointGap * i, this.centerY);
-    //   this.points[i] = point;
-    // }
+    for (let i = 0; i < this.totalPoints; i++) {
+      const point = new Point(this.index + i, this.pointGap * i, this.centerY);
+      this.points[i] = point;
+    }
   }
   draw(ctx) {
     ctx.beginPath();
-    ctx.fillStyle = "#ff0000";
+    //ctx.fillStyle = "#ff0000";
 
-    this.point.update();
+    // this.point.update();
 
-    ctx.arc(this.point.x, this.point.y, 30, 0, 2 * Math.PI);
-    ctx.fill();
-    // ctx.fillStyle = this.color;
-
-    // let prevX = this.points[0].x;
-    // let prevY = this.points[0].y;
-
-    // ctx.moveTo(prevX, prevY);
-
-    // for (let i = 0; i < this.totalPoints; i++) {
-    //   if (i < this.totalPoints - 1) {
-    //     this.points[i].update();
-    //   }
-    //   const cx = (prevX + this.points[i].x) / 2;
-    //   const cy = (prevY + this.points[i].y) / 2;
-
-    //   ctx.lineTo(cx, cy);
-
-    //   prevX = this.points[i].x;
-    //   prevY = this.points[i].y;
-    //}
-
-    // ctx.lineTo(prevX, prevY);
-    // ctx.lineTo(this.stageWidht, this.stageHeight);
-    // ctx.lineTo(this.points[0].x, this.stageHeight);
+    // ctx.arc(this.point.x, this.point.y, 30, 0, 2 * Math.PI);
     // ctx.fill();
-    // ctx.closePath();
+    ctx.fillStyle = this.color;
+
+    let prevX = this.points[0].x;
+    let prevY = this.points[0].y;
+
+    ctx.moveTo(prevX, prevY);
+
+    for (let i = 0; i < this.totalPoints; i++) {
+      if (i < this.totalPoints - 1) {
+        this.points[i].update();
+      }
+      const cx = (prevX + this.points[i].x) / 2;
+      const cy = (prevY + this.points[i].y) / 2;
+
+      ctx.lineTo(cx, cy);
+
+      prevX = this.points[i].x;
+      prevY = this.points[i].y;
+    }
   }
 }
